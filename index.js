6f143f81bdf16d85eeebd87d90fcc679a705dd7d
eliancoronado <cuentaparaelian12@gmail.com>
2025-02-10 16:41:35 -0600

    .

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ app.use(express.json());
 //app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://sizae-frontend.netlify.app", credentials: true }));
 app.use("/images", express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "dist")));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://sizae-frontend.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
